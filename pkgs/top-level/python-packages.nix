c80b40f2ae06f495f482bf354ed3354b81198960
Martin Weinelt <hexa@darmstadt.ccc.de>
2022-01-18 17:06:04 +0100

    python3Packages.pycallgraph: drop
    The project is relying on the `use_2to3` flag that was removed from
    setuptools in version 58. It is also unmaintained since 2018.

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -6520,8 +6520,6 @@ in {
     inherit (pkgs.buildPackages) meson;
   };
 
-  pycallgraph = callPackage ../development/python-modules/pycallgraph { };
-
   py = callPackage ../development/python-modules/py { };
 
   pycangjie = callPackage ../development/python-modules/pycangjie { };
