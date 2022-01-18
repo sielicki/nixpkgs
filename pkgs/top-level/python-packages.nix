9e2e9dd1ec1502138d7b360122bf48406f9fd128
Martin Weinelt <hexa@darmstadt.ccc.de>
2022-01-18 10:26:20 +0100

    python3Packages.lark: rename from lark-parser
    With version 1.0 of lark-parser the name was changed to just lark.

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -4410,7 +4410,7 @@ in {
 
   language-data = callPackage ../development/python-modules/language-data { };
 
-  lark-parser = callPackage ../development/python-modules/lark-parser { };
+  lark = callPackage ../development/python-modules/lark { };
 
   latexcodec = callPackage ../development/python-modules/latexcodec { };
 
