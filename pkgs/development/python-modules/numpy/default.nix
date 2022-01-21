a2274ae5a85b2ccba7494ac093f75fef73e0ef6d
Martin Weinelt <hexa@darmstadt.ccc.de>
2022-01-21 13:12:06 +0100

    Revert "python3Packages.numpy: 1.21.4 -> 1.22.0"
    This reverts commit 7a000c2fe71cff406a6e7d30fa114e8efe1e6174.
    
    This breaks parts of scipy and we need to wait until scipy figures a way
    to deal with it. By extension this breaks alot of reverse dependencies,
    so we revert.

--- pkgs/development/python-modules/numpy/default.nix
+++ pkgs/development/python-modules/numpy/default.nix
@@ -40,14 +40,14 @@ let
   };
 in buildPythonPackage rec {
   pname = "numpy";
-  version = "1.22.0";
+  version = "1.21.4";
   format = "pyproject.toml";
   disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
     extension = "zip";
-    sha256 = "a955e4128ac36797aaffd49ab44ec74a71c11d6938df83b1285492d277db5397";
+    sha256 = "e6c76a87633aa3fa16614b61ccedfae45b91df2767cf097aa9c933932a7ed1e0";
   };
 
   patches = lib.optionals python.hasDistutilsCxxPatch [
